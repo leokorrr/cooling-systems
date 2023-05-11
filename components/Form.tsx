5c6796fdb0b883e4e0b9b8d1d13acf11837b09f5
leokorrr <leokorrr@gmail.com>
2023-05-11 18:11:53 +0200

    Add Form
    Add Form component
    
    Remove unnecessary code from globals

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,5 +1,31 @@
+import { FormFieldType } from '@/types'
+import { FormField } from './FormField'
+
+const FORM_FIELDS: FormFieldType[] = [
+  {
+    label: 'Title:',
+    name: 'title'
+  },
+  {
+    label: 'Description:',
+    name: 'description'
+  },
+  {
+    label: 'Price:',
+    name: 'price'
+  }
+]
+
 export const Form: React.FC = () => {
   return (
-    <form>Form</form>
+    <form>
+      {FORM_FIELDS.map((formField: FormFieldType) => (
+        <FormField
+          key={formField.name}
+          label={formField.label}
+          name={formField.name}
+        />
+      ))}
+    </form>
   )
 }
