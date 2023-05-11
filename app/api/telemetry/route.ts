e70abd0c905000255b67fc21a4fc1e1aad60910a
leokorrr <leokorrr@gmail.com>
2023-05-11 17:55:01 +0200

    Add data validation to telemetry POST endpoint

--- app/api/telemetry/route.ts
+++ app/api/telemetry/route.ts
@@ -1,9 +1,22 @@
 import prisma from '@/lib/prisma'
 import { StorageItem } from '@prisma/client'
 import { NextResponse } from 'next/server'
+import { z } from 'zod'
+
+const storageItemBodySchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  price: z.number()
+})
 
 export async function POST(req: Request) {
   const data = await req.json()
+
+  const validationResult = storageItemBodySchema.safeParse(req.body)
+
+  if (!validationResult.success) {
+    return NextResponse.json({ message: 'Error: Bad payload' }, { status: 400 })
+  }
 
   try {
     const newStorageItem: StorageItem = await prisma.storageItem.create({
@@ -12,6 +25,9 @@ export async function POST(req: Request) {
 
     return NextResponse.json(newStorageItem)
   } catch (error) {
-    return NextResponse.json({ message: 'Error: Adding new storage item' })
+    return NextResponse.json(
+      { message: 'Error: Adding new storage item' },
+      { status: 500 }
+    )
   }
 }
