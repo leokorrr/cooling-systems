9e3438257698b9260da014f1810f7d5b03545e06
leokorrr <leokorrr@gmail.com>
2023-05-12 14:38:15 +0200

    Add margin top to main

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { StorageItemsList } from '@/components/StorageItemsList'
 
 export default function Home() {
   return (
-    <main className='flex w-full justify-center'>
+    <main className='flex w-full justify-center mt-5'>
       <div className='flex w-full max-w-[1720px]'>
         <div className='w-[50%] flex justify-center'>
           <StorageItemsList />
