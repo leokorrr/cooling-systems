a27775a9ce5d09777f7782a2808ea9bdb1337408
leokorrr <leokorrr@gmail.com>
2023-05-12 12:54:12 +0200

    Add GET telemetry endpoint

--- app/api/telemetry/route.ts
+++ app/api/telemetry/route.ts
@@ -9,6 +9,19 @@ const storageItemBodySchema = z.object({
   price: z.number().positive('Price should be positive')
 })
 
+export async function GET() {
+  try {
+    const storageItems: StorageItem[] = await prisma.storageItem.findMany()
+
+    return NextResponse.json(storageItems)
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Error: Getting storage items' },
+      { status: 500 }
+    )
+  }
+}
+
 export async function POST(req: Request) {
   const data = await req.json()
 
