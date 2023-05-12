d0f3fc7da1f1a4101bb146a75aa7808c9380f9c3
leokorrr <leokorrr@gmail.com>
2023-05-12 15:36:58 +0200

    Add Github auth
    Setup Next auth
    
    Add Github provider
    
    Add basic protection for client route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+'use client'
+
 import { Form } from '@/components/Form'
 import { StorageItemsList } from '@/components/StorageItemsList'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data: session } = useSession()
+  if (!session) {
+    return (
+      <>
+        <button className='bg-red-500 text-white' onClick={() => signIn()}>SignIn</button>
+      </>
+    )
+  }
+
   return (
     <main className='flex w-full justify-center mt-5'>
       <div className='flex w-full max-w-[1720px]'>
