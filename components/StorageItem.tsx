39c23e66f38934570da2288f07cafeee7655a951
leokorrr <leokorrr@gmail.com>
2023-05-12 14:34:57 +0200

    Add truncate to title and description in StorageItem

--- components/StorageItem.tsx
+++ components/StorageItem.tsx
@@ -6,9 +6,9 @@ export const StorageItem: React.FC<StorageItemOmitId> = ({
   price
 }) => {
   return (
-    <div className='flex justify-between px-5 h-[50px] items-center text-xl bg-grey-400 drop-shadow-md text-white rounded text-[18px]'>
-      <div>{title}</div>
-      <div>{description}</div>
+    <div className='flex justify-between px-5 h-[50px] items-center text-xl bg-grey-400 drop-shadow-md text-white rounded text-[18px] gap-5'>
+      <div className='truncate'>{title}</div>
+      <div className='truncate'>{description}</div>
       <div>{price}$</div>
     </div>
   )
