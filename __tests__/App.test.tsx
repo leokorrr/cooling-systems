43fde13d60afab2abe1e631d0af1dddb4f0d5d42
leokorrr <leokorrr@gmail.com>
2023-05-13 06:39:13 +0200

    Update renders correctly test
    Add useSession mock

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,6 +2,24 @@ import Home from '@/app/page'
 import { Navbar } from '@/components/Navbar'
 import { render, screen } from '@testing-library/react'
 import { SessionProvider } from 'next-auth/react'
+import '@testing-library/jest-dom'
+import { useSession } from 'next-auth/react'
+import userEvent from '@testing-library/user-event/'
+
+jest.mock('next-auth/react', () => {
+  const originalModule = jest.requireActual('next-auth/react')
+  const mockSession = {
+    expires: new Date(Date.now() + 2 * 86400).toISOString(),
+    user: { username: 'admin' }
+  }
+  return {
+    __esModule: true,
+    ...originalModule,
+    useSession: jest.fn(() => {
+      return { data: mockSession, status: 'authenticated' } // return type is [] in v3 but changed to {} in v4
+    })
+  }
+})
 
 const renderApp = () =>
   render(
@@ -18,9 +36,26 @@ describe('App...', () => {
     const appHeader = screen.getByText(/coolingsystems/i)
     expect(appHeader).toBeInTheDocument()
 
-    const signInButton = screen.getByRole('button', {
-      name: /sign in with github/i
+
+    const titleInput = screen.getByRole('textbox', {
+      name: /title:/i
     })
-    expect(signInButton).toBeInTheDocument()
+    expect(titleInput).toBeInTheDocument()
+
+    const descriptionInput = screen.getByRole('textbox', {
+      name: /description:/i
+    })
+    expect(descriptionInput).toBeInTheDocument()
+
+
+    const priceInput = screen.getByRole('spinbutton', {
+      name: /price:/i
+    })
+    expect(priceInput).toBeInTheDocument()
+
+    const addButton = screen.getByRole('button', {
+      name: /add/i
+    })
+    expect(addButton).toBeInTheDocument()
   })
 })
